a3b1b69347d3ce863ab00738a4e42d2d4a601937
Nuubz <mrmikillll@gmail.com>
2023-02-06 20:33:45 +0100

    trying out signed commits

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,23 +1,23 @@
 import {describe, it} from 'mocha'
 
 describe("database tests", () => {
-    describe("creation test", () => {
+    context("creation test", () => {
         it("should create an accessible database")
         it("should create a file for the database")
         it("should throw 'path is directory' error")
     })
-    describe("#set()", () => {
+    context("#set()", () => {
         it("append to the file and not throw an error")
     })
-    describe("#get()", () => {
+    context("#get()", () => {
         it("should read through the file and return a match")
         it("should read through the file and not return a match")
     })
-    describe("#has()", () => {
+    context("#has()", () => {
         it("should read through the file and find a match")
         it("should read through the file and not find a match")
     })
-    describe("#remove()", () => {
+    context("#remove()", () => {
         it("should remove an entry from the file")
         it("should not remove a non existen entry")
     })
